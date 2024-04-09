3c86ffd54c55c6319739b99cc96964555fbc1a83
Vishwanathanselvamoorthy <viswanathanselvamoorthy@gmail.com>
2024-04-09 20:15:45 +0530

    Done For Mobile UI

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const sidebar = document.querySelector(".sidebar-container");
 const closeSideBar = document.querySelector(".close-btn");
 const openSideBar = document.querySelector(".open-btn");
+const blankSpaceContainer = document.querySelector(".blank");
 
 closeSideBar.addEventListener("click", (e) => {
   e.preventDefault();
   sidebar.classList.add("hide-side-bar");
+  blankSpaceContainer.classList.remove("blank-layer");
 });
 openSideBar.addEventListener("click", (e) => {
   e.preventDefault();
   sidebar.classList.remove("hide-side-bar");
+  blankSpaceContainer.classList.add("blank-layer");
 });
