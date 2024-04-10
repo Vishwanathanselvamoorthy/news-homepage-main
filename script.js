422d69bb3a156badefd9e842940f037978d8c030
Vishwanathanselvamoorthy <viswanathanselvamoorthy@gmail.com>
2024-04-10 15:20:59 +0530

    Done With Main Image Margin Adjustemnt Related To Media Query

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ const openSideBar = document.querySelector(".open-btn");
 const blankSpaceContainer = document.querySelector(".blank");
 const navbarContent = document.querySelector(".navbar-content");
 
-// if (window.innerWidth >= 1024) {
-//   sidebar.classList.add("hidden-class");
-//   openSideBar.classList.add("hidden-class");
-//   navbarContent.classList.remove("hidden-class");
-// }
+
 
 closeSideBar.addEventListener("click", (e) => {
   e.preventDefault();
