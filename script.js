3fc0b916dc1a8339f30a3eda0cc7119cccb66d20
Vishwanathanselvamoorthy <viswanathanselvamoorthy@gmail.com>
2024-04-10 11:34:52 +0530

    .

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const closeSideBar = document.querySelector(".close-btn");
 const openSideBar = document.querySelector(".open-btn");
 const blankSpaceContainer = document.querySelector(".blank");
 
+document.addEventListener("DOMContentLoaded", function () {
+  let mainImg = document.querySelector(".main-img");
+
+  if (window.innerWidth <= 768) {
+    mainImg.src = "./assets/images/image-web-3-mobile.jpg";
+  } else if (window.innerWidth >= 1024) {
+    mainImg.src = "./assets/images/image-web-3-desktop.jpg";
+  }
+});
+
 closeSideBar.addEventListener("click", (e) => {
   e.preventDefault();
   sidebar.classList.add("hide-side-bar");
